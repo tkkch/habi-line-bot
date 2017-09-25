3b6932afaaf42d59d4000891ac0e55339faa7918
habrishi <khunanon2274@gmail.com>
2017-09-25 18:35:29 +0700

    Add files via upload

--- bot.php
+++ bot.php
@@ -1,4 +1,4 @@
-<?php
+﻿<?php
 $access_token = 'IvZukDGBwHdPtKzUbArMXq6nXzok07+ksXt4iIwpcaUulvY2T6RnWKNV7p9a9sOT1gNxb+/juDDUW8UnkoKCclxpc/18io19O2LjIVl9d6M966Zp7hK28QMDsO77QDBqsmvyv41NfkBV5we4a4reJAdB04t89/1O/w1cDnyilFU=';
 
 $proxy = 'velodrome.usefixie.com:80';
@@ -23,7 +23,7 @@ if (!is_null($events['events'])) {
 			// Build message to reply back
 			$messages = [
 				'type' => 'text',
-				'text' => $text
+				'text' => LineBot นะจ๊ะไม่ใช่คน ไม่ต้องตอบกลับบ่อย
 			];
 
 			// Make a POST Request to Messaging API to reply to sender
