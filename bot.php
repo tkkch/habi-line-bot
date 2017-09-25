7ed6a59f3fe9639913c48296104d4971a714d3f6
habrishi <khunanon2274@gmail.com>
2017-09-25 19:45:33 +0700

    Add files via upload

--- bot.php
+++ bot.php
@@ -22,7 +22,7 @@ if (!is_null($events['events'])) {
 			// Build message to reply back
 			$messages = [
 				'type' => 'text',
-				'text' => $text
+				'text' => 'Line Bot นะไม่ใช่คนมีไรพิมพ์ help เลย'
 			];
 
 			// Make a POST Request to Messaging API to reply to sender
